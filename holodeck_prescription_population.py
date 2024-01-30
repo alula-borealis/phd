7028855440a424da9191623c031f12451edb2aea
Laura Burn <lauraburn@talktalk.net>
2024-01-30 17:51:31 +1300

    Working semi-analytic population, check results

--- holodeck_prescription_population.py
+++ holodeck_prescription_population.py
@@ -6,9 +6,9 @@ from astropy.cosmology import WMAP9 as cosmo
 from astropy import units as u
 
 # constants
-m_sun = ap.constants.M_sun
-G = ap.constants.G
-c = ap.constants.c
+m_sun = 1.989e30
+G = 6.6743e-11
+c = 299792458
 pi = np.pi
 
 # variables
@@ -25,15 +25,15 @@ print(massesc)
 # q = m2/m1
 z = 0.05 # redshift to source
 
-vsim = cosmo.comoving_volume(z).si   # Comoving volume
-dc = cosmo.comoving_distance(z).si   # comoving distance (these should be SI)
+vsim = cosmo.comoving_volume(z).si.value   # Comoving volume
+dc = cosmo.comoving_distance(z).si.value   # comoving distance (these should be SI)
 print("vsim", vsim)
 print("dc", dc)
 
 fbins = 100
-f = np.linspace(0.0001, 1, fbins)*u.Hz.si # for population
+freqs = np.linspace(0.0001, 1, fbins) # for population
 # f = 1*u.Hz.si # for single binary
-print("f", f)
+print("f", freqs)
 
 # calculations
 # masses
@@ -44,44 +44,39 @@ mc = ((m1*m2)**(3/5))/(m_total**(1/5)) # chirp mass
 # by an e-folding
 # dt/dln(fr) = fr/(dfr/dt) = tau
 
-# f is the observer-frame frequency to source
-fr = (1 + z)*f # fr is the rest-frame GW freq (twice orbital)
-
-# assuming circular binary driven by GWs
-tau = (5/96)*(((G*massesc)/c**3)**(-5/3))*(2*pi*fr)**(-8/3)
-
-# total GW power emitted
-power = (32/5*G*c**5)*(G*massesc)**(10/3)*(2*pi*fr)**(10/3)
-
-# luminosity distance related to comoving distance
-dl = dc*(1 + z)
-
-# GW strain [Sesana 08]
-hs = (8/10**(1/2))*((G*massesc)**(5/3)/((c**4)*dl))*(2*pi*fr)**(2/3)
-print("hs", hs)
-
-# relating number and number density
-# d^2N/dz dln(fr) = (dnc/dz)(dt/dln(fr))(dz/dt)(dVc/dz)
-
-# dt/dln(fr) is the hardening timescale
-# -the longer binaries spend in an interval, the more binaries expected to find
-# nothing enforces number of binaries is an integer
-
-# dz/dt = H0(1+z)E(z) rate of evolution of the Universe
-
-# dVc/dz = 4pi(c/Ho)(dc^2/E(z))
-
-# hc^2 = \int(from 0 to inf) dz(dnc/dz)hs^2 4pi c dc^2 (1+z)tau
-#### FOR AN INDIVIDUAL BINARY
-
-h2_integrand = (hs**2)*((4*pi*c*(dc**2)*(1+z))/vsim)*tau
-h = np.sqrt(h2_integrand)
-
-print("strain", h)
-
 #### SUMMATION
 ## comoving number-density of sources
 # nc = dN/dVc = N/V_sim
 num = 1e6 # number of sources N
 # vsim - comoving volume of simulation - the same as vc?
 nc = num/vsim # comoving number-density of sources
+
+fr = (1 + z)*freqs # fr is the rest-frame GW freq (twice orbital)
+
+# luminosity distance related to comoving distance
+dl = dc*(1 + z)
+
+def calculate_strain(chirp_masses, fr, dl, vsim, z):
+    total_strain = np.zeros_like(fr)
+
+    for chirp_mass in chirp_masses:
+        # Calculate tau
+        tau = (5/96)*(((G*chirp_mass)/c**3)**(-5/3))*(2*pi*fr)**(-8/3)
+
+        # Calculate hs
+        hs = (8/10**(1/2))*((G*chirp_mass)**(5/3)/((c**4)*dl))*(2*pi*fr)**(2/3)
+
+        # Calculate h2_integrand
+        h2_integrand = (hs**2)*((4*pi*c*(dc**2)*(1+z))/vsim)*tau
+
+        # Accumulate the strain
+        total_strain += np.sqrt(h2_integrand)
+
+    return total_strain
+
+total_strain = calculate_strain(massesc, fr, dl, vsim, z)
+
+plt.loglog(fr, total_strain)
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Gravitational Wave Strain')
+plt.show()
